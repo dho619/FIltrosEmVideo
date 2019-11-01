ae2da07ba41b19f69eec011e9e13d3eb9f676b36
Geovane Barbosa <geovanebarbosacc@hotmail.com>
2019-11-01 16:52:43 -0300

    Ta indo

--- FaceDetectionDlib.py
+++ FaceDetectionDlib.py
@@ -3,100 +3,108 @@ from pathlib import Path
 import numpy as np
 import argparse, dlib, cv2, sys, os
 
+#Processar Video
 def processando_video(video, output, skip):
     vs = cv2.VideoCapture(video)
     read = 0
     processed = 0
 
-    len_folder = len(os.listdir(output))
+    #len_folder = len(os.listdir(output))
 
     while True:
-        (ret, frame) = vs.read()
+        (sucess, frame) = vs.read()
 
-        if not ret:
+        if not sucess:#se nao conseguiu ler
             break
 
         read += 1
 
-        if read % skip != 0:
+        if read != 0 and read % skip != 0:#o numero de pulos antes de fazer outra deteccao
             continue
 
-        path2save = os.path.sep.join([output,
-                "{}.png".format(len_folder+processed)])
+        pathSave = "VideoNew.png"#os.path.sep.join([output,"VideoNew.png"])
 
-        save_faces(frame, path2save)
+        show_result(frame, pathSave)
         processed +=1
 
     vs.release()
     cv2.destroyAllWindows()
 
+#Processar Imagem
+def processando_imagem(imagem, output):
+    if not os.path.isfile(imagem):
+        print('Nao foi possivel ler a Imagem "{}", por favor, tente novamente!'.format(path_image))
+        sys.exit()
+
+    pathSave = 'ImagemFace.png'#os.path.sep.join([output,"ImagemFace.png"])
+
+    show_result(cv2.imread(imagem), pathSave)
+
 #rodar a imagem em 90graus n vezes (times, define o numero de vezes)
-def rotate(image, times):
+def rotate(image, times=1):
     return np.rot90(image, times)
 
-def save_faces(frame, output, verbose=False):
+def show_result(frame, output, verbose=False):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     detector = dlib.get_frontal_face_detector()
-    rects = detector(gray, 0)
+    rects = detector(gray, 0)#tenta achar o rosto da forma que esta
 
     i=1
-
-    #tenta achar o rosto em cada rotacao
+    '''
+    #tenta achar o rosto em outras rotacoes
     while len(rects) is 0 and i<4:
         gray = rotate(gray, 1)
         frame = rotate(frame, 1)#rotaciona o frame, para poder pegar o rosto na posicao certa depois
         rects = detector(gray, 0)
         i+=1
+    '''
     face_image = frame.copy()
     for (_, rect) in enumerate(rects):
-        l_x = int(rect.tl_corner().x)# - rect.tl_corner().x*0.1)
-        t_y = int(rect.tl_corner().y) #- rect.tl_corner().y*0.2)
-        r_x = int(rect.br_corner().x) #+ rect.br_corner().x*0.1)
-        b_y = int(rect.br_corner().y) #+ rect.br_corner().y*0.2)
+        l_x = int(rect.tl_corner().x)
+        t_y = int(rect.tl_corner().y)
+        r_x = int(rect.br_corner().x)
+        b_y = int(rect.br_corner().y)
 
-        face_image = frame[t_y:b_y , l_x:r_x, :]
-        cv2.imwrite(output, face_image)
-        
-    
+        #face_image = frame[t_y:b_y , l_x:r_x, :]
+        cv2.rectangle(face_image, (l_x, t_y), (r_x, b_y), (0, 0, 255), 2)
+
+    #cv2.imwrite(output, face_image)
+    cv2.imshow('Exibicao do Resultado',face_image)
+    cv2.waitKey(1)
 
     if verbose:
         print["[INFO] saved {}".format(path)]
 
-def processando_imagens(input_folder, output):
-    processed = 0
+############################################################################
+############################################################################
+#########-------------Código Principal do Programa-------------#############
+############################################################################
+############################################################################
+ap = argparse.ArgumentParser(
+    prog='Detecção de Face',
+     description='''Esse é um programa de detecção de faces, criado com a biblioteca dlib, como
+     pode ver a frente, você terá a opção de passar video ou imagem para a detecção!''',
+     epilog='''Espero ter lhe ajudado! ^-^''',
+     formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
 
-    len_folder = len(os.listdir(input_folder))
-
-    for name in os.listdir(input_folder):
-        path_image = os.path.join(input_folder, name)
-        if not os.path.isfile(path_image):
-            continue
-
-        path2save = os.path.sep.join([output, 
-            "{}.png".format(len_folder+processed)])
-        
-        save_faces(cv2.imread(path_image), path2save)
-        processed+=1
-
-
-
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--input_video", type=str, required=False, help="path to input video")
-ap.add_argument("-f", "--input_folder", type=str, required=False, help="path to folder imagens")
-ap.add_argument("-o", "--output", type=str, required=True, help="path to output directory of cropped faces")
-ap.add_argument("-s", "--skip", type=int, default=5, help="# of frames to skip before applying face detection")
+ap.add_argument("-v", "--path_video", type=str, required=False, help="Caminho de onde esta o vídeo")
+ap.add_argument("-i", "--path_imagem", type=str, required=False, help="Pasta com a imagem")
+ap.add_argument("-o", "--output", type=str, required=False, help="Diretorio para onde vai a saida, caso seja passado")
+ap.add_argument("-s", "--skip", type=int, default=5, help="Numero de frames que deve pular entre cada aplicação de detecção de face.")
 args = vars(ap.parse_args())
 
-if not args["input_video"] and not args["input_folder"]:
-    print('input_video ou input_folder sao nescessarios!')
+if not args["path_video"] and not args["path_imagem"]:
+    print('Video ou imagem sao nescessarios!')
     sys.exit()
 
-path = Path(args["output"])
-path.mkdir(parents=True, exist_ok=True)
+if args["output"]:
+    path = Path(args["output"])
+    path.mkdir(parents=True, exist_ok=True)
 
-if args["input_video"]:
-    processando_video(args["input_video"], args["output"], args["skip"])
+if args["path_video"]:
+    processando_video(args["path_video"], args["output"], args["skip"])
 
-if args["input_folder"]:
-    processando_imagens(args["input_folder"], args["output"])
+if args["path_imagem"]:
+    processando_imagem(args["path_imagem"], args["output"])
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# FIltrosEmVideo
+# FiltrosEmVideo
 Trabalho de Processamento de Imagem - Filtros em um video
+
+
+Separar biblioteca de leitura e de exibição do video
