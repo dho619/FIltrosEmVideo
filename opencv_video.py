93349e1c61939c5b660659c98194735440333cc3
RafaelMedeiros02 <rafaelsouzamedeiros02@gmail.com>
2019-11-01 18:38:39 -0300

    Comentando codigo

--- opencv_video.py
+++ opencv_video.py
@@ -1,23 +1,28 @@
 import cv2, time, imutils
 
-cap = cv2.VideoCapture('video.avi')
+cap = cv2.VideoCapture('video.avi') # Lendo o video e guardando na variavel cap
 i = 0
-while(cap.isOpened()):
-    if(i==180):
+while(cap.isOpened()):#Enquanto o video esta aberto
+    if(i==180):#Controle para executar apenas 180 frames
         break
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('Tons de Cinza',gray)
+    ret, frame = cap.read() #Pegando a imagem(frame) atual, o ret e o retorno se conseguiu ou nao ler
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #transformando imagem para tom de Cinza
+    cv2.imshow('Tons de Cinza',gray) #exibindo a imagem em tom de cinza
+
+    '''cv2.waitKey(30) vai esperar pelo menos 30milisegundos antes de continuar
+       0xFF == ord('q') se a tecla apertada foi 'q'
+       Isso faz com que todo frame aguarde pelo menos 30milisegundos e se apertar q sai do while
+    '''
     if cv2.waitKey(30) & 0xFF == ord('q'):
         break
     i+=1
 
-filtro = 'Redimensionar Fixo'
+#Para frente irei comentar apenas o que nao foi comentado acima
 while(cap.isOpened()):
     if(i==360):
         break
     ret, frame = cap.read()
-    resized = cv2.resize(frame, (200, 200))
+    resized = cv2.resize('Redimensionar Fixo', (200, 200))  #usa o opencv para redimencionar o video no tamanho passado
     cv2.imshow(filtro,resized)
     if cv2.waitKey(30) & 0xFF == ord('q'):
         break
@@ -27,8 +32,8 @@ while(cap.isOpened()):
     if(i==510):
         break
     ret, frame = cap.read()
-    Senku = frame[120:220, 200:300]
-    cv2.imshow('Senku',Senku)
+    Senku = frame[120:220, 200:300]#recorta uma parte da imagem, que nada mais e do que uma matriz de pixels
+    cv2.imshow('Senku',Senku)      #do pixel 120 ao 220 na vertical e do 200 ao 300 na horizontal
     if cv2.waitKey(30) & 0xFF == ord('q'):
         break
     i+=1
@@ -37,10 +42,10 @@ while(cap.isOpened()):
     if(i==660):
         break
     ret, frame = cap.read()
-    (h, w, d) = frame.shape
-    r = 300.0 / w
-    dim = (300, int(h * r))
-    resized = cv2.resize(frame, dim)
+    (h, w, d) = frame.shape # pegando as dimensoes do frame linhas(height), colunas(width) e dimensoes
+    r = 300.0 / w #pegando a proporcao de 300 para a largura anterior
+    dim = (300, int(h * r)) #cria uma tupla com 300 na primeira posicao e a antiga altura vezes a proporcao na segunda
+    resized = cv2.resize(frame, dim) # usa novamente o opencv para redimencionar
     cv2.imshow('Redimensionar por proporcao',resized)
     if cv2.waitKey(30) & 0xFF == ord('q'):
         break
@@ -50,7 +55,7 @@ while(cap.isOpened()):
     if(i==810):
         break
     ret, frame = cap.read()
-    resized = imutils.resize(frame, width=500)
+    resized = imutils.resize(frame, width=500) #usa o imutils para redimencionar
     cv2.imshow('Redimensionar do Imutils',resized)
     if cv2.waitKey(30) & 0xFF == ord('q'):
         break
